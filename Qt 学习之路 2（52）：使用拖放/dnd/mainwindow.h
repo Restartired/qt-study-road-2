e7033845f0a149f8c745ad7eb2aacdfc9c545eff
Restartired <760549961@qq.com>
2024-10-10 15:00:58 +0800

    dnd

---
+++ Qt 学习之路 2（52）：使用拖放/dnd/mainwindow.h
@@ -0,0 +1,21 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTextEdit>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+protected:
+    void dragEnterEvent(QDragEnterEvent *event);
+    void dropEvent(QDropEvent *event);
+private:
+    bool readFile(const QString &fileName);
+    QTextEdit *textEdit;
+};
+#endif // MAINWINDOW_H
